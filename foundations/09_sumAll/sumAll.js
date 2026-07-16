345dc8f63a90e1aeda053c89bf433eb22bc3ac0f
rhsiam167 <rh.siam167@gmail.com>
2026-07-16 19:03:20 +0600

    completed exercises 9,10 and 11

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if(typeof(a)==='number'&&typeof(b)==='number'&& Number.isInteger(a)&& Number.isInteger(b)&& a>0 && b>0){
+        let arr = [];
+        arr.push(a);
+        arr.push(b);
+        arr.sort((a,b)=>a-b);
+        let sum = 0;
+        for(let i=arr[0];i<=arr[1];i++){
+            sum += i;
+        }
+            return sum;
 
+    }
+    
+    else {return 'ERROR';}
 };
 
 // Do not edit below this line
